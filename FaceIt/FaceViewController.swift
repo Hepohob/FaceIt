ba9169434b1126f40c245d61976f44aa8eae516b
Aleksei Neronov <aleksei.neronov@yahoo.com>
2016-12-17 17:45:24 +0100

    first

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FaceViewController.swift
+//  FaceIt
+//
+//  Created by Aleksei Neronov on 16/12/2016.
+//  Copyright © 2016 Aleksei Neronov. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+
+}
+
